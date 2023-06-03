a8d1f9402af316b3f2f119bb94cb009a537eb49e
swarup3457 <120475037+swarup3457@users.noreply.github.com>
2023-06-03 16:46:26 +0530

    Add files via upload

---
+++ DAY 4 half sub/HS.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:03:33 06/03/2023 
+// Design Name: 
+// Module Name:    HS 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module HS(a,b,diff,borrow
+    );
+	 //dataflow
+	/*input a,b;
+	output diff,borrow;
+	assign diff=a^b;
+	assign borrow=~a&b;
+endmodule*/
+//gate level modeling
+
+input a,b;
+output diff,borrow;
+wire w1;
+xor x1(diff,a,borrow);
+not n1(w1,a);
+and a1(borrow,w1,b);
+//Behavioural modeling
+/*module hs(a,b,diff,borrow);
+input a,b;
+output diff,borrow;
+reg diff,borrow;
+always@(a,b)
+begin
+if(a==0 && b==1)
+begin
+diff=1;
+borrow=1;
+end
+else if(a==1 && b==0)
+begin
+diff=1;
+borrow=0;
+end
+else
+begin
+diff=0;
+borrow=0;
+end
+end
+endmodule  */
+
+
+endmodule
---
+++ DAY 4 half sub/hstb.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:08:46 06/03/2023
+// Design Name:   HS
+// Module Name:   D:/swarup/swarup/hstb.v
+// Project Name:  swarup
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: HS
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module hstb;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire diff;
+	wire borrow;
+
+	// Instantiate the Unit Under Test (UUT)
+	HS uut (
+		.a(a), 
+		.b(b), 
+		.diff(diff), 
+		.borrow(borrow)
+	);
+	initial begin
+	    $monitor("simtime=%g,A=%b,B=%b,diff=%b,borrow=%b",$time,a,b,diff,borrow);
+		 end
+	initial begin
+		// Initialize Inputs
+		a = 0;
+		b = 0;
+
+		// Wait 100 ns for global reset to finish
+	
+        #100;a = 0;
+		b = 1;
+		#100;a = 1;
+		b = 0;
+		#100;a = 1;
+		b = 1;
+		// Add stimulus here
+
+	end
+      
+endmodule
+
