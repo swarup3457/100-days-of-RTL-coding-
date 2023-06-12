9f08667b929cacea034780cae80a73c14387b561
swarup3457 <120475037+swarup3457@users.noreply.github.com>
2023-06-12 22:12:22 +0530

    Add files via upload

---
+++ DAY 17 asyncdff/Dffasync.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:11:05 06/12/2023 
+// Design Name: 
+// Module Name:    Dffasync 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Dffasync(clk,rst,d,q
+    );
+input rst,clk,d;
+output q;
+reg q;
+always@(posedge clk, negedge rst)
+begin
+if(rst==1)
+q=0;
+else
+case(d)
+1'b0:q=1'b0;
+1'b1:q=1'b1;
+endcase
+end
+
+endmodule
---
+++ DAY 17 asyncdff/Dffasynctb.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:12:50 06/12/2023
+// Design Name:   Dffasync
+// Module Name:   D:/swarup/swarup/Dffasynctb.v
+// Project Name:  swarup
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Dffasync
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Dffasynctb;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg d;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	Dffasync uut (
+		.clk(clk), 
+		.rst(rst), 
+		.d(d), 
+		.q(q)
+	);
+
+	initial begin
+	$monitor("$time=%g,clk=%b,rst=%b,d=%b,q=%b",$time,clk,rst,d,q);
+	end
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 1;
+
+		// Wait 100 ns for global reset to finish
+		#100;rst=0;d=0;
+		#100;d=1;
+		#100;rst=1;d=1;
+		#100;rst=0;d=1;
+		end
+		always
+		#50 clk=~clk;
+      
+endmodule
+
