e2b2815523555be79c01ab8c2079b7cc65d540e4
swarup3457 <120475037+swarup3457@users.noreply.github.com>
2023-06-13 22:37:55 +0530

    Add files via upload

---
+++ DAY 18 T f-f/Tfftb.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:08:41 06/13/2023
+// Design Name:   Tflipflop
+// Module Name:   D:/swarup/swarup/Tfftb.v
+// Project Name:  swarup
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: Tflipflop
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module Tfftb;
+
+	// Inputs
+	reg t;
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	Tflipflop uut (
+		.t(t), 
+		.clk(clk), 
+		.reset(reset), 
+		.q(q)
+	);
+initial begin
+	$monitor("time=%g,t=%b,reset=%b,clk=%b,q=%b",$time,t,reset,clk,q);
+	end
+
+	initial begin
+		// Initialize Inputs
+		reset = 1;
+		clk=0;
+
+		// Wait 100 ns for global reset to finish
+		#100;t=0;reset=0;
+		#100;t=1;
+	
+		
+        
+		// Add stimulus here
+
+	end
+	always
+	#50 clk=~clk;
+endmodule
+
---
+++ DAY 18 T f-f/Tflipflop.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:07:10 06/13/2023 
+// Design Name: 
+// Module Name:    Tflipflop 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Tflipflop(t,clk,reset,q
+    );
+input t,clk,reset;
+output q;
+reg q;
+always@(posedge clk)
+begin
+if(reset==1)
+q=0;
+else
+q=~q;
+end
+endmodule
