4fab5e113a21f73b3939b4cf22286dffeeed35f9
swarup3457 <120475037+swarup3457@users.noreply.github.com>
2023-06-13 22:38:20 +0530

    Add files via upload

---
+++ DAY 19 asyncTff/AsyncTff.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    22:21:58 06/13/2023 
+// Design Name: 
+// Module Name:    AsyncTff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module AsyncTff(t,clk,reset,q
+    );
+
+input reset,clk,t;
+output q;
+reg q;
+always@(posedge clk,reset)
+begin
+if(reset==1)
+q=0;
+else
+case(t)
+1'b0:q=q;
+1'b1:q=~q;
+endcase
+end
+endmodule
---
+++ DAY 19 asyncTff/asynctfftb.v
@@ -0,0 +1,65 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:24:06 06/13/2023
+// Design Name:   AsyncTff
+// Module Name:   D:/swarup/swarup/asynctfftb.v
+// Project Name:  swarup
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: AsyncTff
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module asynctfftb;
+
+	// Inputs
+	reg t;
+	reg clk;
+	reg reset;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	AsyncTff uut (
+		.t(t), 
+		.clk(clk), 
+		.reset(reset), 
+		.q(q)
+	);
+
+	initial begin
+	$monitor("time=%g,t=%b,reset=%b,clk=%b,q=%b",$time,t,reset,clk,q);
+	end
+
+	initial begin
+		// Initialize Inputs
+		reset = 1;
+		clk=0;
+
+		// Wait 100 ns for global reset to finish
+		#100;t=0;reset=0;
+		#100;t=1;
+	#100;t=0;reset=1;
+		#100;t=1;reset=0;
+		
+        
+		// Add stimulus here
+
+	end
+	always
+	#50 clk=~clk;
+endmodule
+
