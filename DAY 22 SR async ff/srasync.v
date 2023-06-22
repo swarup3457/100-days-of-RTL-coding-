37606182bbb2408a7e7a1f160fc0adffa48de39a
swarup3457 <120475037+swarup3457@users.noreply.github.com>
2023-06-22 20:53:22 +0530

    Add files via upload

---
+++ DAY 22 SR async ff/srasync.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:33:41 06/22/2023 
+// Design Name: 
+// Module Name:    srasync 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module srasync(clk,reset,s,r,q
+    );
+
+input clk,reset,s,r;
+output q;
+reg  q;
+always@(posedge clk,reset)
+begin
+if(reset)
+q=0;
+else
+case({s,r})
+2'b00:q=q;
+2'b01:q =0;
+2'b10:q =1;
+2'b11:q =1'bz;
+endcase
+end
+endmodule
---
+++ DAY 22 SR async ff/srasynctb.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   20:36:28 06/22/2023
+// Design Name:   srasync
+// Module Name:   D:/swarup/swarup/srasynctb.v
+// Project Name:  swarup
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: srasync
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module srasynctb;
+
+	// Inputs
+	reg clk;
+	reg reset;
+	reg s;
+	reg r;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	srasync uut (
+		.clk(clk), 
+		.reset(reset), 
+		.s(s), 
+		.r(r), 
+		.q(q)
+	);
+
+	initial begin
+   $monitor("time=%g,clk=%b,reset=%b,s=%b,r=%b,q=%b",$time,clk,reset,s,r,q);
+	end
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		reset = 1;
+		// Wait 100 ns for global reset to finish
+		#100;reset = 0;
+		s = 0;
+		r = 0;
+		#50;reset=1; 
+      #100;
+		s = 0;
+		r = 1;  
+		  #100;
+		  reset=0;
+		s = 1;
+		r = 0;  
+		  #100;
+		s = 1;
+		r = 1;  
+		// Add stimulus here
+
+	end
+      always
+      #50 clk=~clk;
+endmodule
+
